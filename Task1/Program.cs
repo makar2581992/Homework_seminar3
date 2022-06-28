4b37076c2b3ab1478b1ed8a313f661e8dfd2e58d
Dmitryi <dima2581992@mail.ru>
2022-06-28 20:49:10 +0300

    Tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// палиндром
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int Str = Prompt("Введите число: ");
+
+string someStr = Str.ToString();
+string rev = new string(someStr.Reverse().ToArray());
+
+int reStr = int.Parse(rev);
+if (Str == reStr)
+{
+    System.Console.WriteLine($"Число {Str} является палиндромом");
+}
+
+else
+{
+    System.Console.WriteLine($"Число {Str} не является палиндромом");
+}
